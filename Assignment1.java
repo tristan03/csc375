6b0cbf3776c8d69c0b4d274713465f1af0231f55
Tristan Allen <tristan9340@gmail.com>
2023-09-13 17:57:27 -0400

    added hashmap to hold station# -> stationInfo & refined code to fit this

--- Assignment1.java
+++ Assignment1.java
@@ -8,47 +8,78 @@ package Assignment1;
     A parallel genetic algorithm for a Facilities Layout problem
  */
 
-import java.awt.*;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
+import java.util.List;
 
 public class Assignment1 {
     public static void main(String[] args) {
         // define dimensions of the factory floor
-        int height = 10;
-        int width = 10;
+        int height = 30;
+        int width = 20;
 
         // create factory floor
         FactoryFloor factoryFloor = new FactoryFloor(height, width);
 
         // create stations
-        Station station = new Station(new int[] {}, "");
+        Station station = new Station("", new int[] {}, "");
+        List<Station> stationList = new ArrayList<>(); // list to hold at least 48 stations
 
-        // randomly generate population, ignoring duplicates
-        int[] stationPosition = randomlyGeneratePopulation(factoryFloor, station);
+        // randomly generate population, TODO: ignoring duplicate positions
+        Map<String, Station> stationMap = randomlyGeneratePopulation(factoryFloor, stationList);
+
+        stationMap.put("Station1", station);
 
 
         //System.out.println(Arrays.toString(randomPosition));
-        printFloor(factoryFloor, stationPosition);
+        //printFloor(factoryFloor, stationPosition);
         System.out.println("Position of station: " + Arrays.toString(station.getPosition()));
     }
 
     // print floor for visualization
 
-    static int[] randomlyGeneratePopulation(FactoryFloor factoryFloor, Station station) {
-        Random random = new Random();
-        int height = factoryFloor.getHeight();
-        int width = factoryFloor.getWidth();
+    static Map<String, Station> randomlyGeneratePopulation(FactoryFloor factoryFloor, List<Station> stationList) {
+        int[] position;
+        Map<String, Station> stationMap = new HashMap<>();
+        Station station = new Station("", new int[]{}, "");
 
-        int randomHeight = random.nextInt(height);
-        int randomWidth = random.nextInt(width);
+        for (int i = 0; i < 49; i++) {
+            Random random = new Random();
+            int height = factoryFloor.getHeight();
+            int width = factoryFloor.getWidth();
 
-        int[] position = {randomHeight, randomWidth};
+            int randomHeight = random.nextInt(height);
+            int randomWidth = random.nextInt(width);
 
-        station.setPosition(position);
+            position = new int[]{randomHeight, randomWidth};
 
-        return position;
+            station.setName("Station" + i);
+            station.setPosition(position);
+
+            stationMap.put(station.getName(), new Station(station.getName(), station.getPosition(), randomlyGenerateFunction()));
+        }
+
+        return stationMap;
     }
+
+    // TODO: probably delete this and make an actual function solution
+    static String randomlyGenerateFunction() {
+        String function1 = "Cutting";
+        String function2 = "Assembly";
+        String function3 = "QualityControl";
+
+        int max = 3;
+        Random random = new Random();
+        int randomFunction = random.nextInt(max);
+
+        if (randomFunction == 1) {
+            return function1;
+        } else if (randomFunction == 2) {
+            return function2;
+        } else {
+            return function3;
+        }
+    }
+
     static void printFloor(FactoryFloor factoryFloor, int[] stationPosition) {
         int height = factoryFloor.getHeight();
         int width = factoryFloor.getWidth();
@@ -92,17 +123,20 @@ class FactoryFloor {
 }
 
 class Station {
+    private String name;
     private int[] position;
     private String function;
 
-    public Station(int[] position, String function) {
+    public Station(String name, int[] position, String function) {
+        this.name = name;
         this.position = position;
         this.function = function;
     }
-
+    String getName() { return name; }
     int[] getPosition() { return position; }
     String getFunction() { return function; }
 
+    void setName(String name) { this.name = name; }
     void setPosition(int[] position) { this.position = position; }
     void setFunction(String function) { this.function = function; }
 
